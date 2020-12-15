bc5eccff1bfaa62baf439fa399ffe5f8aaf7a11e
Erick Rodriguez <germanerick.rodriguez@avianca.com>
2020-12-15 11:58:00 -0600

    1 version, reconoce algunos qr

--- src/app/escanear-qr/escanear-qr.component.ts
+++ src/app/escanear-qr/escanear-qr.component.ts
@@ -1,18 +1,18 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component,  OnInit,  ViewChild } from '@angular/core';
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-escanear-qr',
   templateUrl: './escanear-qr.component.html',
   styleUrls: ['./escanear-qr.component.css']
 })
-export class EscanearQRComponent implements OnInit {
-  @ViewChild('video', { static: true }) videoElement: ElementRef;
-  @ViewChild('canvas', { static: true }) canvas: ElementRef;
 
-  constructor(private renderer: Renderer2) {}
+export class EscanearQRComponent implements OnInit {
+ 
+  @ViewChild('scanner', { static: false })  scanner: ZXingScannerComponent;
 
   ngOnInit(): void {
-    this.startCamera();
+
   }
 
   constraints = {
@@ -21,25 +21,20 @@ export class EscanearQRComponent implements OnInit {
         width: { ideal: 4096 },
         height: { ideal: 2160 }
     }
-};
+  };
 
-capture(){}
+  qrResultString: string;
 
-  startCamera() {
-    if (!!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) { 
- navigator.mediaDevices.getUserMedia(this.constraints).then(this.attachVideo.bind(this)).catch(this.handleError);
-    } else {
-        alert('Sorry, camera not available.');
-    }
-}
+  clearResult(): void {
+    this.qrResultString = null;
+  }
 
+  onCodeResult(resultString: string) {
+    this.qrResultString = resultString;
+  }
 
-handleError(error) {
-  console.log('Error: ', error);
-}
-
-attachVideo(stream) {
-  this.renderer.setProperty(this.videoElement.nativeElement, 'srcObject', stream);
+doSomething(): void {
+  alert(this.scanner.scanSuccess);
 }
 
 
